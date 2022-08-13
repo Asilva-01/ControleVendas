a77985171c55d353d89aec653bd890c1c6eef9bf
Andre Luiz <andre1981luiz@gmail.com>
2022-08-12 22:06:09 -0300

    First Model-Controller-View - Departmento

---
+++ VendasWebMVC_3/Controllers/DepartamentoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC_3.Models;
+
+namespace VendasWebMVC_3.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Name = "Eletronicos" });
+            list.Add(new Departamento { Id = 2, Name = "Moda" });
+
+
+            return View(list);
+        }
+    }
+}
