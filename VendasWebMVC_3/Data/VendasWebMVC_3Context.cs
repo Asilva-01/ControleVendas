30c1b5da478e957603071f9c8584adc8795041bf
Andre Luiz <andre1981luiz@gmail.com>
2022-08-12 22:28:26 -0300

    CRUD Scaffolding

---
+++ VendasWebMVC_3/Data/VendasWebMVC_3Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWebMVC_3.Models
+{
+    public class VendasWebMVC_3Context : DbContext
+    {
+        public VendasWebMVC_3Context (DbContextOptions<VendasWebMVC_3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasWebMVC_3.Models.Departamento> Departamento { get; set; }
+    }
+}
--- VendasWebMVC_3/Startup.cs
+++ VendasWebMVC_3/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VendasWebMVC_3.Models;
 
 namespace VendasWebMVC_3
 {
@@ -33,6 +35,9 @@ namespace VendasWebMVC_3
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VendasWebMVC_3Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("VendasWebMVC_3Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
